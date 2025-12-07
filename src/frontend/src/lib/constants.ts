4d16824305d1cacc543638b4caba3959de31f797
vishnu vishwakarma <vv083150@gmail.com>
2025-12-07 17:03:33 +0530

    typo

--- src/frontend/src/lib/constants.ts
+++ src/frontend/src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = (
     import.meta.env.PROD
         ? import.meta.env.VITE_API_URL
-        : import.meta.env.VITE_API_LOCAL_URL || "http://localhost:3000"
+        : import.meta.env.VITE_LOCAL_API_URL || "http://localhost:3000"
 ) as string;
